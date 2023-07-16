c8200f516a2d453a14f70a25a3b1ba412b450cab
smkim <ksm0057@naver.com>
2023-07-16 19:20:53 +0900

    TextInput & Keyboard

--- firstApp/App.tsx
+++ firstApp/App.tsx
@@ -11,120 +11,47 @@ import {
   RefreshControl,
   FlatList,
   SectionList,
+  TextInput,
 } from 'react-native';
 
 function App(): JSX.Element {
-  // const [TitleIdx, setTitleIdx] = useState(2);
-  const [Items, setItems] = useState([
-    {
-      title: 'Title 1',
-      data: ['Item 1-1', 'Item 1-2'],
-    },
-  ]);
-  const [Refreshing, setRefreshing] = useState(false);
-
-  const DATA = [
-    {
-      title: 'Title 1',
-      data: ['Item 1-1', 'Item 1-2'],
-    },
-  ];
-
-  const onRefresh = () => {
-    // setTitleIdx(TitleIdx + 1);
-    setRefreshing(true);
-    const add_TitleIdx = Items.length + 1;
-    setItems([
-      ...Items,
-      {
-        title: `Title ${add_TitleIdx}`,
-        data: [`Item ${add_TitleIdx}-1`, `Item ${add_TitleIdx}-2`],
-      },
-    ]);
-    setRefreshing(false);
-  };
-
+  const [Name, setName] = useState('');
   return (
-    <SectionList
-      keyExtractor={(item, index) => index.toString()}
-      sections={Items}
-      renderItem={({item}) => <Text style={styles.text}>{item}</Text>}
-      renderSectionHeader={({section}) => (
-        <View style={styles.item}>
-          <Text style={styles.text}>{section.title}</Text>
-        </View>
-      )}
-      refreshControl={
-        <RefreshControl
-          refreshing={Refreshing}
-          onRefresh={onRefresh}
-          colors={['#ff00ff']}
-        />
-      }
-    />
-    // <FlatList
-    //   // numColumns={2}
-    //   // inverted
-    //   // horizontal
-    //   keyExtractor={(item, index) => index.toString()}
-    //   data={Items}
-    //   renderItem={({item}) => (
-    //     <View style={styles.item}>
-    //       <Text style={styles.text}>{item.name}</Text>
-    //     </View>
-    //   )}
-    //   refreshControl={
-    //     <RefreshControl
-    //       refreshing={Refreshing}
-    //       onRefresh={onRefresh}
-    //       colors={['#ff00ff']}
-    //     />
-    //   }
-    // />
-    // <ScrollView
-    //   horizontal={false}
-    //   style={styles.body}
-    //   refreshControl={
-    //     <RefreshControl
-    //       refreshing={Refreshing}
-    //       onRefresh={onRefresh}
-    //       colors={['#ff00ff']}
-    //     />
-    //   }>
-    //   {Items.map(item => {
-    //     return (
-    //       <View style={styles.item} key={item.key}>
-    //         <Text style={styles.text}>{item.item}</Text>
-    //       </View>
-    //     );
-    //   })}
-    // </ScrollView>
+    <View style={styles.body}>
+      <Text style={styles.text}>Please write your name :</Text>
+      <TextInput
+        // multiline
+        style={styles.input}
+        placeholder="e.g. John"
+        onChangeText={value => setName(value)}
+        keyboardType={'default'}
+        maxLength={10}
+        editable={true}
+        secureTextEntry={true}
+      />
+      <Text style={styles.text}>your name is : {Name}</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection: 'column',
     backgroundColor: '#ffffff',
-  },
-  item: {
-    margin: 10,
-    backgroundColor: '#4ae1fa',
-    justifyContent: 'center',
     alignItems: 'center',
   },
-
   text: {
     color: '#000000',
-    fontSize: 45,
-    fontStyle: 'italic',
+    fontSize: 20,
     margin: 10,
-    textTransform: 'uppercase',
   },
-  button: {
-    width: 150,
-    height: 60,
+  input: {
+    width: 200,
+    borderWidth: 1,
+    borderColor: '#555',
+    borderRadius: 5,
+    textAlign: 'center',
+    fontSize: 20,
   },
 });
 
